ca92a7ab7ab8e0f975517c3d21a7249121f76a78
GUILHERME SUGANAME <guilherme.suganame@capgemini.com>
2024-06-25 16:41:51 -0300

    feat: project and first commit

---
+++ src/main/java/br/ce/wcaquino/entidades/Usuario.java
@@ -0,0 +1,35 @@
+package br.ce.wcaquino.entidades;
+
+import java.util.Objects;
+
+public class Usuario {
+
+	private String nome;
+	
+	public Usuario() {}
+	
+	public Usuario(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) return true;
+		if (object == null || getClass() != object.getClass()) return false;
+		Usuario usuario = (Usuario) object;
+		return Objects.equals(nome, usuario.nome);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome);
+	}
+}
---
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,43 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+import java.util.Date;
+
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class LocacaoServiceTest {
+
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
+
+    @Test
+    public void test() {
+        // Arrange
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 1", 2, 5.0);
+
+        // Act
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        // Assert
+        assertEquals(5, locacao.getValor(), 0.01);
+        error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+//        assertThat(locacao.getValor(), is(not(6.0)));
+        error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+    }
+}
